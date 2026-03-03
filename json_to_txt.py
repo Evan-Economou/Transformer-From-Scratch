d823b7d3468249148c3eadc6d32c2b18bd7c31bd
Joe <josephhuston47@Outlook.com>
2026-03-02 22:27:31 -0700

    fixed json parser and modified tokenizer to handle leftover html

--- json_to_txt.py
+++ json_to_txt.py
@@ -7,15 +7,29 @@ data = ''
 with open("data/pres_data.txt", "w") as outfile:
     for file_path in folder_path.iterdir():
         if file_path.is_file():
-            with open(file_path, 'r') as infile:
-                data = json.load(infile)
-                speeches = data["transcript"]
-                infile.close()
-                outfile.write(" " + speeches)
+            try:
+                with open(file_path, 'r') as infile:
+                    data = json.load(infile)
+                    speeches = data["transcript"]
+                    infile.close()
+                    outfile.write(" " + speeches)
+            except Exception as e:
+                print(e)
+                continue
+
     outfile.close() 
 
-            
-                   
+
+# try:
+#     # Code that may raise an exception
+#     result = 10 / 0 
+#     print(result)
+# except ZeroDivisionError:
+#     # Code to run if a specific exception (ZeroDivisionError) occurs
+#     print("Error: Cannot divide by zero!")
+# except Exception as e:
+#     # Code to handle other, more general exceptions and access the error message
+#     print(f"An unexpected error occurred: {e}")   
 
 # for key in data.keys():
 #     print(key)
